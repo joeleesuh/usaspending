d7310131d90139eb9c8a9131ac7f68b3fd6f9556
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-02-24 16:51:21 -0500

    [google-analytics-data-dictionary] adding event

--- src/js/components/bulkDownload/dictionary/DataDictionary.jsx
+++ src/js/components/bulkDownload/dictionary/DataDictionary.jsx
@@ -6,6 +6,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Analytics from 'helpers/analytics/Analytics';
 import { Spreadsheet } from 'components/sharedComponents/icons/Icons';
 import DataDictionaryTable from './table/DataDictionaryTable';
 import DataDictionarySearchBar from "./DataDictionarySearchBar";
@@ -23,37 +24,43 @@ const propTypes = {
     downloadLocation: PropTypes.string
 };
 
-export default class DataDictionary extends React.Component {
-    render() {
-        return (
-            <div className="data-dictionary">
-                <h2 className="data-dictionary__title">Data Dictionary</h2>
-                <p className="data-dictionary__intro">
-                    The data dictionary below shows detailed information about the data available in our download
-                    files, including the definition of each element and its element name on the legacy USAspending.gov
-                    website.
-                </p>
-                <div className="data-dictionary__search-download">
-                    <DataDictionarySearchBar
-                        setSearchString={this.props.setSearchString} />
-                    <div className="data-dictionary__download">
-                        <a
-                            className="data-dictionary__download-link"
-                            href={this.props.downloadLocation}>
-                            <div className="data-dictionary__download-icon">
-                                <Spreadsheet />
-                            </div>
-                        Download
-                        </a>
-                    </div>
-                </div>
-                <div className="data-dictionary__table-wrapper">
-                    <DataDictionaryTable
-                        {...this.props} />
+const DataDictionary = (props) => {
+    const handleDownloadClick = () => {
+        Analytics.event({
+            category: 'Data Dictionary',
+            action: 'Click'
+        });
+    };
+    return (
+        <div className="data-dictionary">
+            <h2 className="data-dictionary__title">Data Dictionary</h2>
+            <p className="data-dictionary__intro">
+                The data dictionary below shows detailed information about the data available in our download
+                files, including the definition of each element and its element name on the legacy USAspending.gov
+                website.
+            </p>
+            <div className="data-dictionary__search-download">
+                <DataDictionarySearchBar
+                    setSearchString={props.setSearchString} />
+                <div className="data-dictionary__download">
+                    <a
+                        onClick={handleDownloadClick}
+                        className="data-dictionary__download-link"
+                        href={props.downloadLocation}>
+                        <div className="data-dictionary__download-icon">
+                            <Spreadsheet />
+                        </div>
+                    Download
+                    </a>
                 </div>
             </div>
-        );
-    }
+            <div className="data-dictionary__table-wrapper">
+                <DataDictionaryTable
+                    {...props} />
+            </div>
+        </div>
+    );
 }
 
 DataDictionary.propTypes = propTypes;
+export default DataDictionary;
